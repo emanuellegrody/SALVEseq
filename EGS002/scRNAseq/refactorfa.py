b3128aa5a39dfde92808a83d98649f7b73321701
emanuellegrody <emanuelle.grody@northwestern.edu>
2023-11-01 19:07:09 -0500

    Working, exported the correct gtf file

--- EGS002/scRNAseq/refactorfa.py
+++ EGS002/scRNAseq/refactorfa.py
@@ -30,28 +30,40 @@ def split_sequence(split_point, header1, header2, input_sequence):
     return sequence1, sequence2
 
 # read in mac239 genome sequence
-fasta_file = "/Volumes/fsmresfiles/Basic_Sciences/CDB/GoyalLab/People/EmmieGrody/Data/Sequencing/20231017_VISER/genomes/mac239.fa"
+fasta_file = "/Users/egy2296/Downloads/mac239.fa"
 with open(fasta_file, "r") as file:
     input_fa = file.read()
 
 # read in gene annotations
-annotation_file = "/Volumes/fsmresfiles/Basic_Sciences/CDB/GoyalLab/People/EmmieGrody/Data/Sequencing/20231017_VISER/genomes/mac239_annotations.xlsx"
+annotation_file = "/Users/egy2296/Library/CloudStorage/OneDrive-NorthwesternUniversity/Data/Sequencing/mac239_annotations.xlsx"
 df = pd.read_excel(annotation_file, header=None)
 headers = df.iloc[0].tolist()
 split_points = [int(point) for point in df.iloc[1].tolist()]
 
 
 outputDirectory = "/Volumes/fsmresfiles/Basic_Sciences/CDB/GoyalLab/People/EmmieGrody/Data/Sequencing/20231017_VISER/genomes/"
-for header, split_point in zip(headers, split_points):
-    # Call the split_sequence() function
-    sequence1, sequence2 = split_sequence(input_fa, split_point, header, header)
+# untested, needs debugging
+# for header, split_point in zip(headers, split_points):
+#     # Call the split_sequence() function
+#     sequence1, sequence2 = split_sequence(input_fa, split_point, header, header)
+#
+#     # Create filenames based on header
+#     filename1 = header + ".fa"
+#     filename2 = header + ".fa"
+#
+#     # Save the sequences to separate files
+#     with open(filename1, "w") as file1, open(filename2, "w") as file2:
+#         file1.write(sequence1)
+#         file2.write(sequence2)
 
-    # Create filenames based on header
-    filename1 = header + ".fa"
-    filename2 = header + ".fa"
 
-    # Save the sequences to separate files
-    with open(filename1, "w") as file1, open(filename2, "w") as file2:
-        file1.write(sequence1)
-        file2.write(sequence2)
-
+# How to export the annotation list as a GTF
+outputFile = outputDirectory + "mac239full.gtf"
+with open(outputFile, "a") as file:
+    start_point = 1
+    for header, split_point in zip(headers, split_points):
+        # Combine header and split_point into a string with a new line
+        line = f'mac239\tunknown\texon\t{start_point}\t{split_point}\t.\t+\t.\tgene_id "{header}"; transcript_id "{header}"; gene_name "{header}"; gene_biotype "protein_coding";\n'
+        # Write the line to the file
+        file.write(line)
+        start_point = 1 + split_point
