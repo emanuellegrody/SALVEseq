7027256731634988e03c43a524e99df389779870
emanuellegrody <emanuelle.grody@northwestern.edu>
2023-11-02 14:48:21 -0500

    Update allocation needs

--- EGS002/VISER/seqIOchunkEGS002.sh
+++ EGS002/VISER/seqIOchunkEGS002.sh
@@ -4,8 +4,8 @@
 #SBATCH --job-name=seqIO
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --mem=40G
-#SBATCH -t 2:00:00
+#SBATCH --mem=25G
+#SBATCH -t 0:30:00
 #SBATCH --output=/projects/b1042/GoyalLab/egrody/20230929_Goyal_P1_BarcodeseqVISER/scripts/logs/seqIO.txt
 #SBATCH --error=/projects/b1042/GoyalLab/egrody/20230929_Goyal_P1_BarcodeseqVISER/scripts/logs/seqIO.err
 #SBATCH --verbose
