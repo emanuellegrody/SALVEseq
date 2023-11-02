d6a4dd08be89e0af6a56c2672db20e7402923130
emanuellegrody <emanuelle.grody@northwestern.edu>
2023-11-02 11:10:48 -0500

    No need for separate .sh submissions as the seqIO pipeline reads in all samples.

--- EGS002/VISER/seqIOchunkEGS002.sh
+++ EGS002/VISER/seqIOchunkEGS002.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #SBATCH -A b1042
 #SBATCH -p genomics
-#SBATCH --job-name=seqIOW0
+#SBATCH --job-name=seqIO
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --mem=30G
-#SBATCH -t 1:00:00
-#SBATCH --output=/projects/b1042/GoyalLab/egrody/20230929_Goyal_P1_BarcodeseqVISER/scripts/logs/seqIOW0.txt
-#SBATCH --error=/projects/b1042/GoyalLab/egrody/20230929_Goyal_P1_BarcodeseqVISER/scripts/logs/seqIOW0.err
+#SBATCH --mem=40G
+#SBATCH -t 2:00:00
+#SBATCH --output=/projects/b1042/GoyalLab/egrody/20230929_Goyal_P1_BarcodeseqVISER/scripts/logs/seqIO.txt
+#SBATCH --error=/projects/b1042/GoyalLab/egrody/20230929_Goyal_P1_BarcodeseqVISER/scripts/logs/seqIO.err
 #SBATCH --verbose
 
 source activate VISER
