c1499c7bbe08faa2e51390b01fde90c8b11d7899
emanuellegrody <emanuelle.grody@northwestern.edu>
2024-02-08 09:39:30 -0600

    updated location of package

---
+++ EGS004/scRNAseq/scPathoQuant_D13.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH -A b1042
+#SBATCH -p genomics
+#SBATCH --job-name=scpathoquant_D13
+#SBATCH -N 1
+#SBATCH -n 8
+#SBATCH --mem=20G
+#SBATCH -t 1:00:00
+#SBATCH --output=/projects/b1042/GoyalLab/egrody/20240131_VISER/scripts/logs/scpathoquant_D13.txt
+#SBATCH --error=/projects/b1042/GoyalLab/egrody/20240131_VISER/scripts/logs/scpathoquant_D13.err
+#SBATCH --verbose
+
+/home/egy2296/packages/scPathoQuant/scpathoquant -10x /projects/b1042/GoyalLab/egrody/20240131_VISER/counts/cellranger_count_D13/ -op /projects/b1042/GoyalLab/egrody/20240131_VISER/scPathoQuant/D13/ -p 8 -p2genome /home/egy2296/packages/scPathoQuant/mac239_genome/
