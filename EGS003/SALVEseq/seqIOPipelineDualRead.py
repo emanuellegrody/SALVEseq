ea70f8b6af28b05f9ca5f99fcf451f9019632803
emanuellegrody <emanuelle.grody@northwestern.edu>
2024-06-06 16:09:53 -0500

    Edited for PL

--- EGS003/SALVEseq/seqIOPipelineDualRead.py
+++ EGS003/SALVEseq/seqIOPipelineDualRead.py
@@ -2,28 +2,31 @@
 print("Let's begin!")
 print("Importing...")
 import gzip
-# import jellyfish
 import numpy as np
 import regex as regex
 from Bio import SeqIO
 import glob
+import os
 
 # Update this block with your specific output directory; input directory is specified in the shell script
 directory = "/projects/b1042/GoyalLab/egrody/20240116_VISER_SALVEseq/analysis/dualRead/"
 
 # Outer loop: iterate through the samples
-samples = ("W0", "W2", "invitro")   # **update
-envstagger = ("GCTC", "A", "CT")    # **update
+samples = ("PL_env", "PL_tat1", "PL_tat2")   # **update
+primers = ("CCAGCAGACCCATATCCAACAGG", "GCTAATACATCTTCTGCCCATATCCAACAGGACC", "GCTAATACATCTTCTGCCCAACAGGACC")
 for i in range(3):
     print("Starting on sample", str(samples[i]))
     # Inner loop: iterate through the second split #
     outputDirectory = directory + samples[i] + "/"
+    if not os.path.exists(outputDirectory):
+        # If it does not exist, create the directory
+        os.makedirs(outputDirectory)
     file_pattern = outputDirectory + samples[i] + "_shavedReads.txt"
     if glob.glob(file_pattern):
         continue
     # Grab two reads
-    inputRead1 = f'5M_{samples[i]}_R1.fastq'
-    inputRead2 = f'5M_{samples[i]}_R2.fastq'
+    inputRead1 = f'{samples[i]}*_R1_001.fastq.gz'
+    inputRead2 = f'{samples[i]}*_R2_001.fastq.gz'
 
     # initializing variables
     read1 = []
@@ -31,23 +34,23 @@ for i in range(3):
     read1Qscore = []
     read2Qscore = []
     joinedRead1Read2 = []
-    envprimer = "CCAGCAGACCCATATCCAACAGG"
-    sumprimerstagger = len(envprimer) + len(envstagger[i])  # **update
+    sampleprimer = primers[i]
+    sumprimerstagger = len(sampleprimer)  # **update
     missingPrimer = []
     badQscore = []
     badTarget = []
     screenedReads = []
     shavedReads = []
-    screenedUtrReads = []
-    shavedUtrReads = []
-    badUtrQscore = []
-    badUtr = []
+    #screenedUtrReads = []  #cmd + / to (un)comment multiple lines
+    #shavedUtrReads = []
+    #badUtrQscore = []
+    #badUtr = []
     # referenceTarget = "ACTGGCCTACCTACAATATGGGTGGAGCTATTTCCATGAGGCGGTCCAGGCCGTCTGGAGATCTGCGACAGAGACTCTTGCGGGCGCGTG"
-    fastQCtrim = 54
+    fastQCtrim = 77   # update
 
-    # take the unzipped read files and parse them into reads; update if input is zipped
-    R1parsed = SeqIO.parse(inputRead1, format="fastq")
-    R2parsed = SeqIO.parse(inputRead2, format="fastq")
+    # take the zipped read files and parse them into reads; update if input is zipped
+    R1parsed = SeqIO.parse(gzip.open(inputRead1, "rt"), format="fastq")
+    R2parsed = SeqIO.parse(gzip.open(inputRead2, "rt"), format="fastq")
 
     # build a dataframe to connect read 1 and read 2 by ID (line 0)
     for record1, record2 in zip(R1parsed, R2parsed):
@@ -73,7 +76,7 @@ for i in range(3):
 
         # Read 2
         # toss reads that don't include our VISER primer
-        if len(regex.findall(rf'({regex.escape(envprimer)}){{e<=4}}',
+        if len(regex.findall(rf'({regex.escape(sampleprimer)}){{e<=4}}',
                              read2Call[0:sumprimerstagger])) == 0:  # update
             missingPrimer.append(read)
             continue
@@ -96,27 +99,27 @@ for i in range(3):
 
         # Read 1
         # toss reads that have a bad quality score
-        if len(Qscore1[np.where(Qscore1[29:] <= 14)]) > 5:    # try many different combinations
-            badUtrQscore.append(read)
-            continue
-        if (len(regex.findall("(TTTTTTTTTT)", read1Call)) < 1) or (len(regex.findall("(NN)", read1Call)) > 0):
-            badUtr.append(read)
-            continue
-        # remove poly(dT)
-        last_index = utr.rfind("TTTTTTTTTT")
-        shaved_utr = np.array([cellID, UMI, utr[last_index + len("TTTTTTTTTT"):]])
-        # toss zero-length reads
-        if len(utr[last_index + len("TTTTTTTTTT"):]) == 0:
-            continue
-
-        screenedUtrReads.append(read)
-        shavedUtrReads.append(shaved_utr)
+        # if len(Qscore1[np.where(Qscore1[29:] <= 14)]) > 5:    # try many different combinations
+        #     badUtrQscore.append(read)
+        #     continue
+        # if (len(regex.findall("(TTTTTTTTTT)", read1Call)) < 1) or (len(regex.findall("(NN)", read1Call)) > 0):
+        #     badUtr.append(read)
+        #     continue
+        # # remove poly(dT)
+        # last_index = utr.rfind("TTTTTTTTTT")
+        # shaved_utr = np.array([cellID, UMI, utr[last_index + len("TTTTTTTTTT"):]])
+        # # toss zero-length reads
+        # if len(utr[last_index + len("TTTTTTTTTT"):]) == 0:
+        #     continue
+        #
+        # screenedUtrReads.append(read)
+        # shavedUtrReads.append(shaved_utr)
 
     uniqueScreenedReads = np.unique(screenedReads, axis=0)
     uniqueShavedReads = np.unique(shavedReads, axis=0)
 
-    uniqueScreenedUtrReads = np.unique(screenedUtrReads, axis=0)
-    uniqueShavedUtrReads = np.unique(shavedUtrReads, axis=0)
+    # uniqueScreenedUtrReads = np.unique(screenedUtrReads, axis=0)
+    # uniqueShavedUtrReads = np.unique(shavedUtrReads, axis=0)
 
     # Print summary file
     summaryFile = open(outputDirectory + samples[i] + "_summaryFile.txt", "w")
@@ -129,13 +132,13 @@ for i in range(3):
     summaryFile.write("total unique screenedReads reads \t%d" % len(uniqueScreenedReads) + "\n")
     summaryFile.write("total shavedReads reads \t\t%d" % len(shavedReads) + "\n")
     summaryFile.write("total unique shavedReads reads \t\t%d" % len(uniqueShavedReads) + "\n")
-    summaryFile.write("Read1:\n")
-    summaryFile.write("total badUtrQscore reads \t\t%d" % len(badUtrQscore) + "\n")
-    summaryFile.write("total badUtr reads \t\t\t%d" % len(badUtr) + "\n")
-    summaryFile.write("total screenedUtrReads reads \t\t%d" % len(screenedUtrReads) + "\n")
-    summaryFile.write("total unique screenedUtrReads reads \t%d" % len(uniqueScreenedUtrReads) + "\n")
-    summaryFile.write("total shavedReads reads \t\t%d" % len(shavedUtrReads) + "\n")
-    summaryFile.write("total unique shavedUtrReads reads \t%d" % len(uniqueShavedUtrReads) + "\n")
+    # summaryFile.write("Read1:\n")
+    # summaryFile.write("total badUtrQscore reads \t\t%d" % len(badUtrQscore) + "\n")
+    # summaryFile.write("total badUtr reads \t\t\t%d" % len(badUtr) + "\n")
+    # summaryFile.write("total screenedUtrReads reads \t\t%d" % len(screenedUtrReads) + "\n")
+    # summaryFile.write("total unique screenedUtrReads reads \t%d" % len(uniqueScreenedUtrReads) + "\n")
+    # summaryFile.write("total shavedReads reads \t\t%d" % len(shavedUtrReads) + "\n")
+    # summaryFile.write("total unique shavedUtrReads reads \t%d" % len(uniqueShavedUtrReads) + "\n")
     summaryFile.close()
 
     # Save outputs to text files for further analysis
@@ -150,14 +153,14 @@ for i in range(3):
                fmt='%s',
                header="cellID,UMI,target", comments="")
 
-    # Read 1
-    np.savetxt(outputDirectory + samples[i] + "_shavedUtrReads.txt", shavedUtrReads, delimiter=",", fmt='%s',
-               header="cellID,UMI,utr", comments="")
-    np.savetxt(outputDirectory + samples[i] + "_badUtrQscore.txt", badUtrQscore, fmt='%s')
-    np.savetxt(outputDirectory + samples[i] + "_badUtr.txt", badUtr, fmt='%s')
-    np.savetxt(outputDirectory + samples[i] + "_uniqueScreenedUtrReads.txt", uniqueScreenedUtrReads, fmt='%s')
-    np.savetxt(outputDirectory + samples[i] + "_uniqueShavedUtrReads.txt", uniqueShavedUtrReads, delimiter=",",
-               fmt='%s',
-               header="cellID,UMI,utr", comments="")
+    # # Read 1
+    # np.savetxt(outputDirectory + samples[i] + "_shavedUtrReads.txt", shavedUtrReads, delimiter=",", fmt='%s',
+    #            header="cellID,UMI,utr", comments="")
+    # np.savetxt(outputDirectory + samples[i] + "_badUtrQscore.txt", badUtrQscore, fmt='%s')
+    # np.savetxt(outputDirectory + samples[i] + "_badUtr.txt", badUtr, fmt='%s')
+    # np.savetxt(outputDirectory + samples[i] + "_uniqueScreenedUtrReads.txt", uniqueScreenedUtrReads, fmt='%s')
+    # np.savetxt(outputDirectory + samples[i] + "_uniqueShavedUtrReads.txt", uniqueShavedUtrReads, delimiter=",",
+    #            fmt='%s',
+    #            header="cellID,UMI,utr", comments="")
 
 print("Done!")
