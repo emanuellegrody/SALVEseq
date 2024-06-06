222d675bbaea4484b790873b2776ecab42e3d0a1
emanuellegrody <emanuelle.grody@northwestern.edu>
2024-06-06 16:37:21 -0500

    Edited for PL

---
+++ EGS003/SALVEseq/seqIOEGS003_PLtat1.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH -A b1042
+#SBATCH -p genomics-himem
+#SBATCH --job-name=seqIO_tat1
+#SBATCH -N 1
+#SBATCH -n 4
+#SBATCH --mem=200G
+#SBATCH -t 20:00:00
+#SBATCH --output=/projects/b1042/GoyalLab/egrody/20240116_VISER_SALVEseq/scripts/logs/seqIO_tat1.txt
+#SBATCH --error=/projects/b1042/GoyalLab/egrody/20240116_VISER_SALVEseq/scripts/logs/seqIO_tat1.err
+#SBATCH --verbose
+
+source activate VISER
+cd /projects/b1042/GoyalLab/egrody/20240116_VISER_SALVEseq/bcl2fastq/
+
+python /projects/b1042/GoyalLab/egrody/20240116_VISER_SALVEseq/scripts/seqIOPipelineDualRead.py "PL_tat1"
